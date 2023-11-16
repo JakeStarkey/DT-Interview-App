d69a33b8140e3adaf866798a3e31ab996f18d4f6
Jacob Starkey <73000476+JakeStarkey@users.noreply.github.com>
2023-11-15 17:49:26 -0700

    Initial Commit

---
+++ dtmadOrderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dtmadOrderApp
+//
+//  Created by jacob starkey on 11/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dtmadOrderApp/dtmadOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dtmadOrderAppApp.swift
+//  dtmadOrderApp
+//
+//  Created by jacob starkey on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct dtmadOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
